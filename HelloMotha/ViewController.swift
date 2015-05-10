76fc5ffb592cc0a81ba9e60c408211057080e333
kauemendes <kaue.mendes@gmail.com>
2015-05-10 18:33:51 -0300

    Hello World do Swift

--- HelloMotha/ViewController.swift
+++ HelloMotha/ViewController.swift
@@ -10,14 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var text1Label: UILabel!
+    @IBOutlet weak var textView: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var x = sum(2, parB: 3)
+        println("Valor do x: \(x)")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func onClick(sender: UIButton) {
+        if( textView.text == "ricardo" ){
+            text1Label.text = "Deu RUIM"
+        } else {
+            text1Label.text = "Você pode!"
+        }
+    }
+    
+    func sum( parA: Int, parB: Int ) -> Int{
+        return parA + parB
     }
 
 
